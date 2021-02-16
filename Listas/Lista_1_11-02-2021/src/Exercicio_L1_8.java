7ae9e60a82892fc48df807c7953d5ca644b6544b
ana.alves <ana.alves@7comm.com.br>
2021-02-16 12:15:31 -0300

    primeira lista de exercicios

---
+++ Listas/Lista_1_11-02-2021/src/Exercicio_L1_8.java
@@ -0,0 +1,17 @@
+public class Exercicio_L1_8 {
+
+	public static void main(String[] args) {
+		int soma = 200;
+		System.out.print("A soma de todos os números pares de 1 a 200 é igual a " + somaPares(soma));
+	}
+
+	private static int somaPares(int num) {
+		if (num == 0)
+			return 0;
+		if (num % 2 == 0)
+			return num + somaPares(num - 1);
+		
+		return somaPares(num - 1);
+	}
+
+}

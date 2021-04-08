eff33f7528a7f24082d6bb7d7cbeebe6b284a514
anabalves <ana.alves41@fatec.sp.gov.br>
2021-04-07 22:59:21 -0300

    Lista de exercicios 6

---
+++ Listas/6/Lista_6_25_03_2021/src/exercicio_L6_3_ALUNO/NO.java
@@ -0,0 +1,24 @@
+package exercicio_L6_3_ALUNO;
+
+public class NO {
+
+	private NO proximo;
+	private Aluno aluno;
+
+	public NO getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(NO proximo) {
+		this.proximo = proximo;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+}
---
+++ Material/Aula_6_18_03_2021/Lista de Exercícios 5 - Corrigida/ListaDeExercicios5/src/Exercicio3/NO.java
@@ -0,0 +1,21 @@
+package Exercicio3;
+/**
+ *
+ * @author Fabio
+ */
+public class NO {
+	public Aluno aluno;
+	public NO prox;
+	
+
+	public NO(Aluno aluno){
+		this.aluno=aluno;
+		prox=null;
+	}
+}
+
+
+
+
+
+
